6dfbb9105e4f029e63e9aea2001bb787a697cbc0
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-02-23 12:39:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import time
 import re
 import sys
 import traceback
-import Zaid.modules.sql.users_sql as sql
+import Verixa.modules.sql.users_sql as sql
 from sys import argv
 from typing import Optional
 from telegram import __version__ as peler
 from platform import python_version as memek
-from Zaid import (
+from Verixa import (
     ALLOW_EXCL,
     CERT_PATH,
     DONATION_LINK,
@@ -32,9 +32,9 @@ from Zaid import (
 
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
-from Zaid.modules import ALL_MODULES
-from Zaid.modules.helper_funcs.chat_status import is_user_admin
-from Zaid.modules.helper_funcs.misc import paginate_modules
+from Verixa.modules import ALL_MODULES
+from Verixa.modules.helper_funcs.chat_status import is_user_admin
+from Verixa.modules.helper_funcs.misc import paginate_modules
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.error import (
     BadRequest,
