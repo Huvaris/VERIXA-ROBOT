c9d4a8023836f771f9d93c9f1e5dabfe0bfdf402
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-03-01 21:47:44 +0530

    Update mongo.py

--- Verixa/mongo.py
+++ Verixa/mongo.py
@@ -3,19 +3,19 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from Zaid import MONGO_DB_URI
+from Verixa import MONGO_DB_URI
 
 
 MONGO_PORT = "27017"
 MONGO_DB_URI = "mongodb+srv://ZAID2:ZAID2@cluster0.plap4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-MONGO_DB = "ZaidRobot"
+MONGO_DB = "VERIXAROBOT"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, 27017)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, 27017)
 db = motor[MONGO_DB]
-db = client["ZaidRobot"]
+db = client["VERIXAROBOT"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
