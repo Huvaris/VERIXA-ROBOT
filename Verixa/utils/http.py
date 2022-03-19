60f69997fee19d60403d1201531531508a92f7f4
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-03-19 14:21:27 +0530

    Update http.py

--- Verixa/utils/http.py
+++ Verixa/utils/http.py
@@ -1,6 +1,6 @@
 """
 MIT License
-Copyright (c) 2021 TheHamkerCat
+Copyright (c) 2021 Verixa Robot
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -19,7 +19,7 @@ SOFTWARE.
 """
 from asyncio import gather
 
-from Zaid import aiohttpsession as session
+from Verixa import aiohttpsession as session
 
 
 async def get(url: str, *args, **kwargs):
