b719d22511edd53db90d65658e7b659e77f7680c
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-03-25 11:17:08 +0530

    Update msg_types.py

--- Verixa/modules/helper_funcs/msg_types.py
+++ Verixa/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from Zaid.modules.helper_funcs.string_handling import button_markdown_parser
+from Verixa.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
