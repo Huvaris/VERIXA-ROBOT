6ba1d59a0db4ae21ee7bdfdf880926747cb73c41
Sᴜᴍɪᴛ Yᴀᴅᴀᴠ <91660827+Sumit9969@users.noreply.github.com>
2022-03-25 11:16:13 +0530

    Update __init__.py

--- Verixa/modules/sql/__init__.py
+++ Verixa/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from Zaid import DB_URI
+from Verixa import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
